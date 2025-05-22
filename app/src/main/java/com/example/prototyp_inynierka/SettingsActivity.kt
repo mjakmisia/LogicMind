4c274dca469048c63d8fc4e72cdd4b5e1dc3bbe0
mjakmisia <chalincia@gmail.com>
2025-05-22 16:06:06 +0200

    Aktualizacja okna ustawień

--- app/src/main/java/com/example/prototyp_inynierka/SettingsActivity.kt
+++ app/src/main/java/com/example/prototyp_inynierka/SettingsActivity.kt
@@ -1,7 +1,10 @@
 package com.example.prototyp_inynierka
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -15,6 +18,33 @@ class SettingsActivity : AppCompatActivity() {
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.selectedItemId = R.id.nav_settings
+
+        val langPl = findViewById<LinearLayout>(R.id.langPl)
+        val langEn = findViewById<LinearLayout>(R.id.langEn)
+
+        val textLangPl = langPl.findViewById<TextView>(R.id.textLangPl)
+        val textLangEn = langEn.findViewById<TextView>(R.id.textLangEn)
+
+        langPl.setOnClickListener {
+            langPl.setBackgroundResource(R.drawable.lang_button_selected)
+            textLangPl.setTextColor(Color.BLACK)
+
+            langEn.setBackgroundResource(R.drawable.lang_button_unselected)
+            textLangEn.setTextColor(Color.GRAY)
+
+            // TODO: Zmień język na PL
+        }
+
+        langEn.setOnClickListener {
+            langEn.setBackgroundResource(R.drawable.lang_button_selected)
+            textLangEn.setTextColor(Color.BLACK)
+
+            langPl.setBackgroundResource(R.drawable.lang_button_unselected)
+            textLangPl.setTextColor(Color.GRAY)
+
+            // TODO: Zmień język na EN
+        }
+
 
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
