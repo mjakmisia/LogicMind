479465a4492313c390bdb8724d1d50d1313a697a
Margriell <s99673@pollub.edu.pl>
2025-06-09 17:31:31 +0200

    Wyeliminowanie błędu wywalającego grę

--- app/src/main/java/com/example/prototyp_inynierka/MainActivity.kt
+++ app/src/main/java/com/example/prototyp_inynierka/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.util.Locale
 
