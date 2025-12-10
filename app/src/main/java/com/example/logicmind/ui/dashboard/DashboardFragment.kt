316cee1dbc0416d8be8fc1067f4e55dafb47f791
Margriell <s99673@pollub.edu.pl>
2025-12-10 19:33:24 +0100

    Czyszczenie projektu z nieużywanych zasobów

--- app/src/main/java/com/example/logicmind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/logicmind/ui/dashboard/DashboardFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.logicmind.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
@@ -18,16 +16,9 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
-
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
